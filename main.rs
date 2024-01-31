71ec21c194623b96c941839d6f0a6f5e7b86ba48
duncanproctor <duncpro@icloud.com>
2024-01-31 13:04:03 -0500

    sync

--- main.rs
+++ main.rs
@@ -8,10 +8,8 @@ fn main() {
     use matrix::VecMatrix;
     use echelon::reduced_row_echelon;
     let mut m: VecMatrix<Rational> = matrix::literal(&[
-        [1, 0, -2, 3, -5, 6, 0],
-        [0, 0, 1, 0, 0, 4, 0],
-        [0, 0, 0, 0, 1, -4, 0],
-        [0, 0, 0, 0, 0, 0, 0]
+        [2, -1],
+        [-8, 4]  
     ]);
     println!("{}", m);
     reduced_row_echelon(&mut m);
